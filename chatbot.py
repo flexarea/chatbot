e2ca5cdfc74b0b58ea268e9ae235a1fb233d4e20
esdras <esdrasntuyenabo40@gmail.com>
2024-05-23 08:17:15 +0200

    remove comments

--- chatbot.py
+++ chatbot.py
@@ -57,5 +57,3 @@ def main():
 if __name__ == "__main__":
     main()
 
-# Discover NLP course materials authored by Julie Medero, Xanda Schofield, and Richard Wicentowski
-# This work is licensed under a Creative Commons Attribution-ShareAlike 2.0 Generic License# https://creativecommons.org/licenses/by-sa/2.0/
